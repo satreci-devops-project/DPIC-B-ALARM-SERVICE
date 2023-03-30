eb65995bd5d38af43a6294fd609e3c46126b1ae7
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-30 21:23:53 +0900

    chore : env.properties를 읽는 config

--- src/main/java/dpicb/notification/config/PropertyConfig.java
+++ src/main/java/dpicb/notification/config/PropertyConfig.java
@@ -1,2 +1,9 @@
-package dpicb.notification.config;public class PropertyConfig {
+package dpicb.notification.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/properties/env.properties")
+public class PropertyConfig {
 }
