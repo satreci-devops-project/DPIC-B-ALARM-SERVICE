e26c08daeb59ef8d1d7be97ca98c22aef7d0307b
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-30 15:31:19 +0900

    init

---
+++ src/test/java/dpicb/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package dpicb.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
